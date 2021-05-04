8f389b33a152046b0ce809070a5b6648921bde8c
Isuri97 <isurim@embla.asia>
2021-05-04 21:08:51 +0530

    Added files

---
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.listview;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    String mTitle[] = {"Edge", "Clips", "OCR", "Tflite"};
+    String mDescription[] = {"Edge Description", "Clips Description", "OCR Description", "Tflite Description"};
+    int images[] = {R.drawable.edge, R.drawable.clips, R.drawable.ocr, R.drawable.tflite};
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+
+        MyAdapter adapter = new MyAdapter(this, mTitle, mDescription, images);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0) {
+                    Toast.makeText(MainActivity.this, "Edge Description", Toast.LENGTH_SHORT).show();
+                }
+                if (position == 0) {
+                    Toast.makeText(MainActivity.this, "Clips Description", Toast.LENGTH_SHORT).show();
+                }
+                if (position == 0) {
+                    Toast.makeText(MainActivity.this, "OCR Description", Toast.LENGTH_SHORT).show();
+                }
+                if (position == 0) {
+                    Toast.makeText(MainActivity.this, "Tflite  Description", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    class MyAdapter extends ArrayAdapter<String> {
+        Context context;
+        String rTitle[];
+        String rDescription[];
+        int rImages[];
+
+        MyAdapter(Context c, String title[],String description[], int img[]){
+            super(c, R.layout.row, R.id.textView1, title);
+            this.context = c;
+            this.rTitle = title;
+            this.rDescription = description;
+            this.rImages = img;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent){
+            LayoutInflater layoutInflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View row = layoutInflater.inflate(R.layout.row, parent, false);
+            ImageView images = row.findViewById(R.id.image);
+            TextView myTitle = row.findViewById(R.id.textView1);
+            TextView myDescription = row.findViewById(R.id.textView2);
+
+            // now set our resources on views
+            images.setImageResource(rImages[position]);
+            myTitle.setText(rTitle[position]);
+            myDescription.setText(rDescription[position]);
+
+            return row;
+
+        }
+    }
+
+}
